c12c6af662de79a578d536457dca5c6c2906b516
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-09 12:35:23 +0530

    Create ex1.java

---
+++ PatternPrograms/ex1.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class AreaofRectangle {
+	public static void main (String[] args) {
+	 for(int i=0; i<=5; i++){
+	     for(int j=0;j<=i; j++){
+	         System.out.print("*");
+	     }
+	     System.out.println();
+	 }
+	}
+}
