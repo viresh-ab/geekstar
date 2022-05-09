c68eebdc4fcec85e0194aff3fbb063b03ff3b7b5
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-09 20:28:15 +0530

    Create ex1.java

---
+++ Classes/Day2/ex1.java
@@ -0,0 +1,12 @@
+// "static void main" must be defined in a public class.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(5<6);
+        System.out.println(8>9);
+        System.out.println(5==5);
+        System.out.println(5<=6);
+        System.out.println(5<=5);
+        System.out.println(5!=6);
+        
+    }
+}
