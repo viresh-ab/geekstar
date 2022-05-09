6a753202688215a120af2602da1914ac23430ec3
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-09 14:09:42 +0530

    Update AreaofCircle.java

--- JavaBasic/AreaofCircle.java
+++ JavaBasic/AreaofCircle.java
@@ -1,1 +1,13 @@
+import java.io.*;
+import java.util.*;
 
+class AreaofCircle {
+	public static void main (String[] args) {
+	 Scanner scan = new Scanner(System.in);
+        int radius = scan.nextInt();
+        System.out.println("radius : "+radius);
+        int pi=3.142;
+        int a = pi*(radius*radius);
+        System.out.println("Area of circle = pi*r^2:"+a);
+	}
+}
