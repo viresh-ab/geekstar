737552c94140c892353f2db7d3e55fb000c303e6
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-09 21:38:48 +0530

    Create ex5.java

---
+++ Classes/Day2/ex5.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Ex{
+    public static void main(String[] args) {
+        int n=80;
+            System.out.println(n>60 || n>70);    
+            System.out.println(n>60 && n>70);    
+            System.out.println(n>60 || n>50);
+            System.out.println(!(n>68));
+    }
+}
