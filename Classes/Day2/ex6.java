4ad5a4abdad485e324d5b1d23b37f041e0f45fa7
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-09 21:47:18 +0530

    Create ex6.java

---
+++ Classes/Day2/ex6.java
@@ -0,0 +1,16 @@
+// ASCII values
+import java.util.*;
+public class Ex{
+    public static void main(String[] args) {
+        char ch='A';
+        int ascii =ch;
+            System.out.println(ascii); 
+//         upper or lower case
+        if(ascii==65){
+            System.out.println("Upper Case"); 
+        }
+        else{
+            System.out.println("Lower Case"); 
+        }
+    }
+}
