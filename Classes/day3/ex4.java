e4a1e378d56ffc2621552ee811926f497b74d4d8
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-10 20:46:31 +0530

    Create ex4.java

---
+++ Classes/day3/ex4.java
@@ -0,0 +1,11 @@
+// to print A-z from asci to char
+public class Main {
+    public static void main(String[] args) {
+  
+        for(int i=65; i<91; i++){
+             char c = (char)i;
+            System.out.print(c);
+        }
+
+    }
+}
