8cce709d29038a89a09d9acd06891921c8812f02
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-10 20:49:48 +0530

    Update ex4.java

--- Classes/day3/ex4.java
+++ Classes/day3/ex4.java
@@ -1,10 +1,16 @@
 // to print A-z from asci to char
+
 public class Main {
     public static void main(String[] args) {
   
         for(int i=65; i<91; i++){
              char c = (char)i;
-            System.out.print(c);
+            System.out.print(c+" ");
+        }
+        System.out.println();
+        for(int j=97; j<123; j++){
+             char b = (char)j;
+            System.out.print(b+" ");
         }
 
     }
