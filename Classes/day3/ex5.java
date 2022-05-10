218aa48c37dc0f9a585fca73b1962ca64b704cd8
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-10 21:22:32 +0530

    Update ex5.java

--- Classes/day3/ex5.java
+++ Classes/day3/ex5.java
@@ -1,1 +1,18 @@
 
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+  Scanner scan= new Scanner(System.in);
+        int count =0;
+        int n= scan.nextInt();
+        for(int i=1; i<=n; i++){
+            if(i%3==0){
+                count=count+1;
+            System.out.print(i);
+            }
+                                System.out.println();
+            System.out.println(count);
+        }
+
+    }
+}
