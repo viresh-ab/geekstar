c70fce736e1537a7e2a91af99ddbdbaf7c742efc
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-10 20:14:24 +0530

    Update ex1.java

--- Classes/day3/ex1.java
+++ Classes/day3/ex1.java
@@ -1,1 +1,14 @@
 
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        int n = scan.nextInt();
+        if(n%2==0){
+        System.out.println("even");
+        }
+        else{
+         System.out.println("odd");   
+        }
+    }
+}
