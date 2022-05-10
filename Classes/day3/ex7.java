69dae87fc2754c8868102e2764974a985a01ed50
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-10 21:47:48 +0530

    Create ex7.java

---
+++ Classes/day3/ex7.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+    Scanner scan= new Scanner(System.in);
+    int n= scan.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                System.out.println(i+"  "+j);
+            }
+        }
+    }
+}
