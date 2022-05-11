a58c18de47c85ba29e28c0771a6e00c4132e891c
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-11 20:47:47 +0530

    Create ex6.java

---
+++ Classes/day4/ex6.java
@@ -0,0 +1,38 @@
+    import java.util.*;
+    public class Main {
+        public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    int n=scan.nextInt();
+        for(int i=1; i<=n; i++){
+             for(int k=1; k<i+1; k++){
+                    System.out.print(k+" ");
+            }
+
+             System.out.println();
+        }
+        }
+//     }
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+      
+     import java.util.*;
+    public class Main {
+        public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    int n=scan.nextInt();
+        for(int i=1; i<=n; i++){
+             for(int k=1; k<i+1; k++){
+                    System.out.print(i+" ");
+            }
+
+             System.out.println();
+        }
+        }
+    }
+      
+// 1 
+// 2 2 
+// 3 3 3 
+// 4 4 4 4 
