1dc7a160c014e1e2643a2b7c4393787722ef8e5f
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-11 11:43:36 +0530

    Update hw3.java

--- Classes/day3/hw3.java
+++ Classes/day3/hw3.java
@@ -1,15 +1,1 @@
-// count multiples of 3  n 5 for n times
 
-import java.util.*;
-public class Main {
-    public static void main(String[] args) {
-        int count =0;
-    Scanner scan = new Scanner(System.in);
-    int n = scan.nextInt();
-    for(int i=1; i<=n; i++){
-         System.out.println(i*3+"   "+i*5);
-        count++;
-        }
-    System.out.println("count"+count);
-    }
-}
