244be03a46a041cf3860b9761b625d47175cdb16
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-11 21:32:42 +0530

    Create ex7.java

---
+++ Classes/day4/ex7.java
@@ -0,0 +1,13 @@
+//   function and methods
+    import java.util.*;
+    public class Main {
+        static int add(int a, int b){
+            return a+b;
+        }
+        public static void main(String[] args) {
+          int a=5;
+          int b=9;
+          int sum=add(a,b);
+          System.out.println(sum);
+        }
+    }
