34e26b0dae5f548fc51a75db47548942f7b355fc
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-11 20:27:17 +0530

    Update ex2.java

--- Classes/day4/ex2.java
+++ Classes/day4/ex2.java
@@ -16,3 +16,18 @@ int n=scan.nextInt();
 // * * 
 // * * * 
 // * * * * 
+
+    import java.util.*;
+    public class Main {
+        public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    int n=scan.nextInt();
+        for(int i=1; i<=n; i++){
+             for(int k=1; k<i+1; k++){
+                    System.out.print("* ");
+            }
+
+             System.out.println();
+        }
+        }
+    }
