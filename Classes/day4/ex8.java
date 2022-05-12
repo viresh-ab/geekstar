f56c981e95f5ebca5fa20959c7acb27261675b2a
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-12 12:56:30 +0530

    Update ex8.java

--- Classes/day4/ex8.java
+++ Classes/day4/ex8.java
@@ -1,1 +1,14 @@
-
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int c=97;
+        for(int i=0; i<=4; i++){
+            for(int j=0; j<i+1;j++){
+                System.out.print((char)c+" ");
+            }
+            System.out.println();
+                            c++;
+        }
+    }
+}
