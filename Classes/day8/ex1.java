af016f15a1679160af9f664789aded24e2b9cfbd
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-18 20:16:22 +0530

    Create ex1.java

---
+++ Classes/day8/ex1.java
@@ -0,0 +1,24 @@
+// "static void main" must be defined in a public class.
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+        int n=scan.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1;j<=i;j++){
+            System.out.print("  ");
+            }
+            for(int j=i;j<=n;j++){
+            System.out.print("* ");
+            }
+            
+        System.out.println();
+        }
+    }
+} 
+
+ * * * * * 
+    * * * * 
+      * * * 
+        * * 
+          * 
