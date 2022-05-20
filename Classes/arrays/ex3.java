23d1fd33fd9c54a2f6047e51266cfdd72a4a429c
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-20 20:58:26 +0530

    Create ex3.java

---
+++ Classes/arrays/ex3.java
@@ -0,0 +1,8 @@
+package arrays;
+
+public class Ex4 {
+	public static void main(String[] args) {
+		float[] arr = {(float) 2.3, (float)5.5, (float) 5.6};
+		System.out.println(arr[0]+", "+arr[1]+", "+arr[2]);
+		}
+}
