2a964abfa80bf8e664fe0700b98272a5b3ad802f
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-20 20:56:12 +0530

    Create ex1.java

---
+++ Classes/arrays/ex1.java
@@ -0,0 +1,19 @@
+// arrays
+public class Ex1 {
+	public static void main(String[] args) {
+//		Integer elements
+		int[] arr =new int[3];
+		arr[0]=5;
+		arr[1]=2;
+		arr[2]=3;
+		System.out.println(arr[0]+", "+arr[1]+", "+arr[2]);
+//      Float type array elements
+		float[] arr1 =new float[3];
+		arr1[0]=5.1f;
+		arr1[1]=2.1f;
+		arr1[2]=3.1f;
+		System.out.println(arr1[0]+", "+arr1[1]+", "+arr1[2]);
+		
+	}
+
+}
