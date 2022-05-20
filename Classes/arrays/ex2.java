ed11843804ca02cc798b3ba29bbdbd60dfea8db7
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-20 20:56:45 +0530

    Create ex2.java

---
+++ Classes/arrays/ex2.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class Ex2 {
+	public static void main(String[] args) {
+		int[] arr =new int[4];
+		arr[0]=5;
+		arr[1]=4;
+		arr[2]=3;
+		arr[3]=2;
+		for(int i=0; i<=arr.length; i++) {
+			System.out.println(arr[i]);	
+		}
+	}
+}
