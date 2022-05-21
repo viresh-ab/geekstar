eb68dc00f1b556a2ca6862fbccf7922cfc1878e9
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-21 21:23:29 +0530

    Update ex8.java

--- Classes/arrays/ex8.java
+++ Classes/arrays/ex8.java
@@ -1,15 +1,21 @@
-// WAP to take input of an array and find the sum
+// WAP to take input of an array and find the sum & product
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         int[] a = new int[n]; 
         int sum=0;
+        int pro=1;
          for(int i=0; i<n; i++){
          a[i]=scan.nextInt();
          }
         for(int i=0; i<n; i++){
             sum = sum+a[i];
+//             sum+=a[i];
+        }
+        for(int i=0; i<n; i++){
+            pro = pro*a[i];
+//             pro*=a[i];
         }
         System.out.println(sum);
     }
