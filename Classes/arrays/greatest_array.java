bb6fd38b3b8796efe8803f3f8cd4e62e594b10c6
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-23 21:13:19 +0530

    Create greatest_array.java

---
+++ Classes/arrays/greatest_array.java
@@ -0,0 +1,15 @@
+public class Main {
+    static int greatestArray(int[] a){
+        int max=a[0];
+           for(int i=0;i<a.length; i++){
+               if(a[i]>max)
+                   max=a[i];
+           }
+        return max;
+        }
+    public static void main(String[] args) {
+        int[] a ={2,3,4,5,6,1};
+        int c=greatestArray(a);
+        System.out.println(c);
+    }
+}
