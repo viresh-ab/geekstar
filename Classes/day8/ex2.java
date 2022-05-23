64f98f879e8d114d24ee8da0eae689e886ee3e86
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-23 12:53:49 +0530

    Update ex2.java

--- Classes/day8/ex2.java
+++ Classes/day8/ex2.java
@@ -1,49 +1,46 @@
-import java.io.*;
-import java.util.*;
-
-public class Solution {
-
+//          * 
+//         * * 
+//        * * * 
+//       * * * * 
+//      * * * * * 
+//     * * * * * * 
+//    * * * * * * * 
+//   * * * * * * * * 
+//  * * * * * * * * * 
+// * * * * * * * * * * 
+// * * * * * * * * * * 
+//  * * * * * * * * * 
+//   * * * * * * * * 
+//    * * * * * * * 
+//     * * * * * * 
+//      * * * * * 
+//       * * * * 
+//        * * * 
+//         * * 
+//          * 
+// ///////////////////////////////////////////////////////////
+public class Main {
     public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    int n = scan.nextInt();
-     for (int i=0; i<n; i++){
-            for (int j=n-i; j>1; j--){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for(int i=0; i<n; i++){
+            for(int j=n-i; j>1; j-- ){
                 System.out.print(" ");
             }
-            for (int j=0; j<=i; j++ ){
+            for(int j=0; j<=i; j++ ){
                 System.out.print("* ");
             }
             System.out.println();
         }
-        for (int i=0; i<n; i++){
-            for (int j=0; j<i; j++){
+        int m =n-1;
+         for (int i=0; i<m; i++){
+            for (int j=0; j<=i; j++){
                 System.out.print(" ");
             }
-            for (int j=n-1; j>=i; j-- ){
+            for (int j=m-1; j>=i; j-- ){
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
 }
-
-         * 
-        * * 
-       * * * 
-      * * * * 
-     * * * * * 
-    * * * * * * 
-   * * * * * * * 
-  * * * * * * * * 
- * * * * * * * * * 
-* * * * * * * * * * 
-* * * * * * * * * * 
- * * * * * * * * * 
-  * * * * * * * * 
-   * * * * * * * 
-    * * * * * * 
-     * * * * * 
-      * * * * 
-       * * * 
-        * * 
-         * 
