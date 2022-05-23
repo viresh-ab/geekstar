1d69576cb7c7e1de1f90fa31624fff4e4358a626
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-23 21:15:31 +0530

    Update greatest_array.java

--- Classes/arrays/greatest_array.java
+++ Classes/arrays/greatest_array.java
@@ -1,6 +1,7 @@
 public class Main {
     static int greatestArray(int[] a){
         int max=a[0];
+//                 int max=Integer.MIN_VALUE;
            for(int i=0;i<a.length; i++){
                if(a[i]>max)
                    max=a[i];
