74906412e01d2a68d33b785a9052513a266cccac
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-24 12:18:06 +0530

    Create ex1.java

---
+++ Classes/while/ex1.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+     int n=1;
+        while(n<=10){
+            System.out.print(n+" ");
+            n++;
+        }
+    }
+}
