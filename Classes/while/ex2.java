a7dc7851d2d89c40851ca6e41398cd74eb71cd3a
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-24 12:20:24 +0530

    Create ex2.java

---
+++ Classes/while/ex2.java
@@ -0,0 +1,8 @@
+ // setting the infinite while loop by passing true to the condition  
+public class Main {
+    public static void main(String[] args) {
+        while(true){
+            System.out.println("infinate while loop");
+        }
+    }
+}
