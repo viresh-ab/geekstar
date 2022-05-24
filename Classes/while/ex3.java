9efeb9604f5ccd396ad3fbfa91ef4657a18b7276
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-24 12:23:13 +0530

    Create ex3.java

---
+++ Classes/while/ex3.java
@@ -0,0 +1,10 @@
+ // to display 1 t0 5 
+public class Main {
+    public static void main(String[] args) {
+        int i =1, n=5;
+        while(i<=n){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
