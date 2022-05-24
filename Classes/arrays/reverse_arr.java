18fc0c115febc42c08d7a5ea2f48c4539bee9e67
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-24 12:05:31 +0530

    Update reverse_arr.java

--- Classes/arrays/reverse_arr.java
+++ Classes/arrays/reverse_arr.java
@@ -16,3 +16,23 @@ public class Main {
         }
     }
 }
+
+////////////////////////////////////////////////////////////
+
+public class Main {
+    static void reverse(int[] a, int n){
+        int i, k, t;
+    for(i=0; i<n/2; i++){
+        t=a[i];
+        a[i]=a[n-i-1];
+        a[n-i-1]=t;
+    }
+    for(k=0; k<n; k++){
+     System.out.print(a[k]+" ");   
+    }
+    }
+    public static void main(String[] args){
+     int[] a = {2,4,6,8,0,9,7,5,3,1};
+     reverse(a, a.length);
+    }
+}
