140fe2533149be81d272cf84c0a7472e6128878e
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-25 11:46:15 +0530

    Update selection_sort.java

--- Classes/arrays/selection_sort.java
+++ Classes/arrays/selection_sort.java
@@ -21,3 +21,36 @@ public class Main {
       }
     }
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// "static void main" must be defined in a public class.
+import java.util.*;
+public class Main {
+  static void staticSort(int[] a){
+    for(int j=0; j<a.length; j++){
+      int fixed = j;
+          for(int i=0; i<a.length; i++){
+            if(a[fixed]>a[i]){
+              int temp = a[fixed];
+              a[fixed]=a[i];
+              a[i]=temp;
+            }
+          }
+      }
+  }
+ 
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int n = scan.nextInt();
+      int[] a = new int[n];
+      for(int i=0; i<n; i++){
+        a[i]=scan.nextInt();
+      }
+      staticSort(a);
+      for(int i=0; i<n; i++){
+        System.out.print(a[i]+" ");
+      }
+      
+    }
+}
