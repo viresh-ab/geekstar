4afe63b4960061038c792a7c5b4fd4a4bf2559a8
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-25 20:34:38 +0530

    Create sorting_adday_push _elements.java

---
+++ Classes/arrays/sorting_adday_push _elements.java
@@ -0,0 +1,18 @@
+// push element in a sorted array at it's sorted position
+public class Main {
+    public static void main(String[] args) {
+        int[] a= {1,3,5,6,7,4};
+        int j = a.length-2;
+        while(a[j]>a[j+1]){
+            int temp =a[j+1];
+            a[j+1]=a[j];
+            a[j]=temp;
+            j--;
+        }
+        int i=0;
+        while(i<a.length){
+            System.out.print(a[i]+ " ");
+            i++;
+        }
+    }
+}
