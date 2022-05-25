2b435fe08b5b383fceb74f0e20ab3d02cec80759
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-25 11:27:26 +0530

    Update bubble_sort.java

--- Classes/arrays/bubble_sort.java
+++ Classes/arrays/bubble_sort.java
@@ -1,21 +1,22 @@
-// Check the sorted have duplicate or not?
+// bubble sorting array
+
 public class Main {
-    static void BubbleSort(int[] a){
-        int temp=Integer.MIN_VALUE;
-        for(int i=0; i<a.length;i++){
-            for(int j=0; j<a.length-1-i;j++){
-            if(a[j]>a[j+1])
-               temp = a[j+1];
-                a[j+1]=a[j];
-                a[j]=temp;
+    static void bubbleSort(int[] a){
+        for(int j=0;j<a.length-1;j++){
+            for(int i=0;i<a.length-1-j;i++){
+                if(a[i]>a[i+1]){
+                    int temp = a[i+1];
+                    a[i+1] = a[i];
+                    a[i] = temp;
+                }
             }
         }
     }
     public static void main(String[] args) {
-        int[] a={1,2,3,4,5,5};    
-        BubbleSort(a);
-        for(int i=0; i<a.length;i++){
-        System.out.println(a[i]+" ");
-        }
+        int[] a = {1,5,2,6,4};
+        bubbleSort(a);
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        } 
     }
 }
