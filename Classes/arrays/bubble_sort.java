2b66f95dec502fdc60d29a20b604e5ae7c0e3e6b
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-25 11:40:54 +0530

    Update bubble_sort.java

--- Classes/arrays/bubble_sort.java
+++ Classes/arrays/bubble_sort.java
@@ -20,3 +20,34 @@ public class Main {
         } 
     }
 }
+
+// bubble sort for user input
+
+// "static void main" must be defined in a public class.
+import java.util.*;
+public class Main {
+  static void bubbleSort(int[] a){
+    for(int j=0; j<a.length-1; j++){
+          for(int i=0; i<a.length-1-j; i++){
+            if(a[i]>a[i+1]){
+              int temp=a[i+1];
+              a[i+1]=a[i];
+              a[i]=temp;
+            }
+          }
+      }
+  }
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int n = scan.nextInt();
+      int[] a = new int[n];
+      for(int i=0; i<n; i++){
+        a[i]=scan.nextInt();
+      }
+      bubbleSort(a);
+      for(int i=0; i<n; i++){
+        System.out.print(a[i]+" ");
+      }
+      
+    }
+}
