dbbc84c216df00b5907b1e05075c9d44f432eec3
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-25 21:02:31 +0530

    Update sorting_adday_push _elements.java

--- Classes/arrays/sorting_adday_push _elements.java
+++ Classes/arrays/sorting_adday_push _elements.java
@@ -16,3 +16,72 @@ public class Main {
         }
     }
 }
+
+//push in a sorted array from right in decrement order
+public class Main {
+    public static void main(String[] args) {
+        int[] a= {4,5,3,2,1};
+        int j = a.length-4;
+        while(j>=0 && a[j]>a[j-1]){
+            int temp =a[j-1];
+            a[j-1]=a[j];
+            a[j]=temp;
+            j++;
+        }
+        int i=0;
+        while(i<a.length){
+            System.out.print(a[i]+ " ");
+            i++;
+        }
+    }
+}
+
+//push in a sorted array from right
+public class Main {
+    static void push(int[] a){
+        for(int i=0; i<a.length; i++){
+            int j = i-1;
+        while(j>=0 && a[j]>a[j+1]){
+            int temp =a[j+1];
+            a[j+1]=a[j];
+            a[j]=temp;
+            j--;
+        }
+        }
+    }
+    public static void main(String[] args) {
+        int[] a= {4,5,3,2,1};
+        
+        push(a);
+        int i=0;
+        while(i<a.length){
+            System.out.print(a[i]+ " ");
+            i++;
+        }
+    }
+}
+
+//push in a sorted array from right
+public class Main {
+    static void push(int[] a){
+        for(int i=0; i<a.length; i++){
+           for(int j=0; j<a.length-1; j++){
+               if(a[j]>a[j+1]){
+                   int temp =a[j+1];
+                   a[j+1]=a[j];
+                   a[j]=temp;
+               }
+        }
+    }
+    }
+    public static void main(String[] args) {
+        int[] a= {4,5,3,2,1};
+        
+        push(a);
+        int i=0;
+        while(i<a.length){
+            System.out.print(a[i]+ " ");
+            i++;
+        }
+    }
+}
