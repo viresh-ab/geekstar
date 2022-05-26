332d3e39199039b1cae1da0b88625e84bf75e885
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-05-26 16:35:54 +0530

    Update check_prime_array.java

--- Classes/arrays/check_prime_array.java
+++ Classes/arrays/check_prime_array.java
@@ -21,3 +21,32 @@ public class Main {
         System.out.print(count);
     }
 }
+///////////////////////////////////////////////////////////////////////
+import java.util.*;
+public class Main {
+  static boolean checkPrime(int n){
+        int count = 0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0)
+                count++;
+        }
+        if(count==2)
+            return true;
+        return false;
+    }
+    public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      int m= in.nextInt();
+      int[] a = new int[m];
+      for(int j=0; j<m; j++){
+        a[j]=in.nextInt();
+      }
+      int count = 0; 
+       for(int i=0; i<m; i++){
+         if(checkPrime(a[i])==true){
+         count++;
+         }
+      }
+          System.out.println(count);
+    }
+}
