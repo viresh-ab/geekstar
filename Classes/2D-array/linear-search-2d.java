c2be99d8faa9594208c11438ab97b888952f482e
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-06-08 17:09:09 +0530

    Update linear-search-2d.java

--- Classes/2D-array/linear-search-2d.java
+++ Classes/2D-array/linear-search-2d.java
@@ -1,1 +1,32 @@
 
+// linear search in 2d array
+public class Main {
+  static int[] linearsearch(int[][] a, int e){
+    int n=a.length;
+    int m=a[0].length;
+    for(int j=0; j<n;j++){
+      for(int i=0; i<m;i++){
+         if(a[j][i]==e){
+           int[] ind={j,i};
+           return ind;
+         }
+      }
+      }
+    int[] ind={-1,-1};
+    return ind;
+  }
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int m= scan.nextInt();
+      int n= scan.nextInt();
+      int ele=5;
+      int[][] arr= new int[m][n];
+      for(int j=0; j<m;j++){
+      for(int i=0; i<n;i++){
+       arr[j][i]=scan.nextInt();
+      }
+      }
+      int[] index=linearsearch(arr,ele);
+    System.out.println(index[0]+" "+index[1]);
+    }
+}
