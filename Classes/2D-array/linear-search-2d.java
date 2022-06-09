beafef6f1d52c97537a333150d9ae2681e5eeec4
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-06-09 12:57:20 +0530

    Update linear-search-2d.java

--- Classes/2D-array/linear-search-2d.java
+++ Classes/2D-array/linear-search-2d.java
@@ -30,3 +30,43 @@ public class Main {
     System.out.println(index[0]+" "+index[1]);
     }
 }
+
+
+/////////////////////////////////////////////////////////////////
+
+// print diagonal2 elements
+import java.util.*;
+public class Main {
+  static int[] diagonals2optimized(int[][] a, int b){
+    int n=a.length;
+    int m=a[0].length;
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+          if(a[i][j]==b){
+            int[] index={i,j};
+            return index;
+          }
+        }
+      }
+    int[] index={-1,-1};
+    return index;
+  }
+  public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int m= scan.nextInt();
+      int n= scan.nextInt();
+      int[][] arr1= new int[m][n];
+      for(int j=0; j<m;j++){
+      for(int i=0; i<n;i++){
+       arr1[j][i]=scan.nextInt();
+      }
+      }
+    int ele=20;
+  
+      int[] index=diagonals2optimized(arr1, ele);
+   for(int i=0; i<index.length;i++){
+       System.out.println(index[i]);
+      }
+    }
+}
