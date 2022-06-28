d4e0a81cdcbd563a8c295757a5ad8f0429ba479f
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-06-28 20:56:50 +0530

    Create ex1.java

---
+++ Classes/oops/ex1.java
@@ -0,0 +1,13 @@
+
+class Human {
+    void walk(){
+        System.out.println("...walk....");
+    }
+    
+}
+public class main {
+    public static void main(String[] args) {
+        Human h=new Human();
+        h.walk();
+    }
+}
