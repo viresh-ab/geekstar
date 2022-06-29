9fcd7f94acee68d78c9fab7f0f292bf234df026b
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-06-29 14:21:55 +0530

    Create chararray.java

---
+++ Classes/strings/chararray.java
@@ -0,0 +1,9 @@
+// Character array
+public class Main {
+    public static void main(String[] args) {
+      char[] ch = {'a','b','c','d'};
+      for(int i=0; i<ch.length; i++){
+                System.out.println(ch[i]+" ");
+      }
+    }
+}
