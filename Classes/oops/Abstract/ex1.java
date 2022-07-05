a0d048a042c2d47e185f0b1ff816d4bd6984a0f2
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-07-05 21:21:28 +0530

    Update ex1.java

--- Classes/oops/Abstract/ex1.java
+++ Classes/oops/Abstract/ex1.java
@@ -1,1 +1,36 @@
 
+abstract class Animal{
+    public abstract void run();
+    public abstract void race();
+}
+class Rabbit extends Animal{
+    @Override
+    public void run(){
+        System.out.println("Fast");
+    }
+    @Override
+    public void race(){
+        System.out.println("Looser");
+    }
+}
+class Torrtoile extends Animal{
+    @Override
+    public void run(){
+        System.out.println("Slow");
+    }
+    @Override
+    public void race(){
+        System.out.println("Winner");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+       Rabbit r = new Rabbit();
+        Torrtoile t = new Torrtoile();
+        r.run();
+        r.race();
+        System.out.println("=============================");
+        t.run();
+        t.race();
+    }
+}
