27abafd92c7141bbee7e6390b680b851669c2af5
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-07-07 21:32:09 +0530

    Update al5.java

--- Classes/Collections/arraylist/al5.java
+++ Classes/Collections/arraylist/al5.java
@@ -20,3 +20,22 @@ public class Main {
         System.out.println(linearsearch(a,target));
     }
 }
+// ==========================================================================
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        a.add(4);
+        a.add(5);
+        a.add(8);
+        int target = 3;
+        for(int i=0; i<a.size();i++){
+            if(a.get(i)==target){
+                System.out.println(i);
+            }
+        }
+    }
+}
