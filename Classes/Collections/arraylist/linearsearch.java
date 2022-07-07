1224fd5facd62a4ad28911e374ac93ab6e643292
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-07-07 21:47:59 +0530

    Update and rename al5.java to linearsearch.java

--- Classes/Collections/arraylist/linearsearch.java
+++ Classes/Collections/arraylist/linearsearch.java
@@ -39,3 +39,26 @@ public class Main {
         }
     }
 }
+// =====================================================================
+import java.util.*;
+public class Main {
+        static int linearsearch(ArrayList a, int t){
+            for(int i=0; i<a.size();i++){
+                if(a.get(i).equals(t)){
+                   return i;
+                }
+            }
+            return -1;
+        }
+      public static void main(String[] args) {
+          Scanner scan = new Scanner(System.in);
+          int n=scan.nextInt();
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        for(int i=0; i<n; i++){
+            int x =scan.nextInt();
+            a.add(x);
+        }
+        int target = 3;
+        System.out.println("index: "+linearsearch(a,target));
+    }
+}
