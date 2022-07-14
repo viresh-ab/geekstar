c124f27bc0246d7841f52539860bece55db1f162
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2022-07-14 21:37:48 +0530

    Update hm2.java

--- Classes/Collections/hm2.java
+++ Classes/Collections/hm2.java
@@ -20,3 +20,28 @@ import java.util.*;
         }
         }
     }
+// ////////////////////////////////////////////////////////////////
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> a = new ArrayList<>();
+        int n = scan.nextInt();
+        for(int i=0; i<n; i++){
+            a.add(scan.nextInt());
+        }
+        HashMap<Integer,Integer> h =new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(h.containsKey(a.get(i))){
+                h.put(a.get(i),h.get(a.get(i))+1);
+            }
+            else{
+             h.put(a.get(i),1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> e : h.entrySet()){
+            System.out.println(e.getKey() + " -> " + e.getValue());
+        }
+    }
+}
+
